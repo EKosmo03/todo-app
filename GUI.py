aedae9aad7fa6a7bd89fba97f6c7bc7343467783
AbrahamG <abeghani01@gmail.com>
2023-08-15 13:02:14 -0700

    Implement edit button

--- GUI.py
+++ GUI.py
@@ -4,10 +4,17 @@ import PySimpleGUI as sg
 label= sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter To-Do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos('todos_gui.txt'), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
+
+
 # layout=[[label, input_box]]) this puts it on one line horizontally and above is 2 seperate rows for the displayment of them #
 # each list within the layout list is its own row #
 while True:
@@ -20,6 +27,19 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos, 'todos_gui.txt')
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos('todos_gui.txt')
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            functions.write_todos(todos, 'todos_gui.txt')
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
